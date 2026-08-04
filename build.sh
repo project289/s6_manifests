ab80d6d910a11d5d01baec7fde7530dea373290d
freewriter27654 <hassanryk25@gmail.com>
2026-08-04 22:16:05 +0500

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p .repo/local_manifests
-curl -o .repo/local_manifests/zeroltexx.xml https://raw.githubusercontent.com/project289/s6_manifests/refs/heads/main/zeroltexx.xml
-/opt/crave/resync.sh
-source build/envsetup.sh
-lunch lineage_zeroltexx-userdebug
-make bacon
